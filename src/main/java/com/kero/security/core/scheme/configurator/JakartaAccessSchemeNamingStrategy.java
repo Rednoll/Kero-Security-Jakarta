2acbfef01a645d8e23922ac14220e0231162e4d3
Kerovazzky <rednoll12@gmail.com>
2020-10-06 15:04:12 +0300

    wip

---
+++ src/main/java/com/kero/security/core/scheme/configurator/JakartaAccessSchemeNamingStrategy.java
@@ -0,0 +1,27 @@
+package com.kero.security.core.scheme.configurator;
+
+import javax.persistence.Entity;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.kero.security.core.scheme.strategy.DefaultAccessSchemeNamingStrategy;
+
+public class JakartaAccessSchemeNamingStrategy extends DefaultAccessSchemeNamingStrategy {
+
+	private static Logger LOGGER = LoggerFactory.getLogger("Kero-Security-Jakarta");
+	
+	@Override
+	public String getName(Class<?> rawType) {
+
+		if(!rawType.isAnnotationPresent(Entity.class)) return getName(rawType);
+
+		Entity annotation = rawType.getAnnotation(Entity.class);
+		
+		String name = annotation.name();
+		
+		LOGGER.debug("Fetch scheme aliase: \""+name+"\" from @Entity");
+	
+		return name;
+	}
+}
--- src/test/java/com/kero/security/core/scheme/configurator/JakartaNameAccessSchemeConfiguratorTest.java
+++ src/test/java/com/kero/security/core/scheme/configurator/JakartaNameAccessSchemeConfiguratorTest.java
@@ -4,20 +4,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import com.kero.security.core.scheme.AccessScheme;
-import com.kero.security.core.scheme.ClassAccessScheme;
-
 public class JakartaNameAccessSchemeConfiguratorTest {
 
 	@Test
 	public void test() {
 		
-		JakartaNameAccessSchemeConfigurator conf = new JakartaNameAccessSchemeConfigurator();
+		JakartaAccessSchemeNamingStrategy conf = new JakartaAccessSchemeNamingStrategy();
 		
-		AccessScheme scheme = new ClassAccessScheme(null, JakartaNameTestEntity.class);
+		String name = conf.getName(JakartaNameTestEntity.class);
 		
-		conf.configure(scheme);
-		
-		assertEquals(scheme.getAliase(), "Jakarta");
+		assertEquals(name, "Jakarta");
 	}
 }
