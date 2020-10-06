3b1f1b4aca08a10a23c0116f2aae292dac8f1389
Kerovazzky <rednoll12@gmail.com>
2020-10-06 11:32:32 +0300

    add Logger

--- src/main/java/com/kero/security/core/scheme/configurator/JakartaNameAccessSchemeConfigurator.java
+++ src/main/java/com/kero/security/core/scheme/configurator/JakartaNameAccessSchemeConfigurator.java
@@ -2,10 +2,15 @@ package com.kero.security.core.scheme.configurator;
 
 import javax.persistence.Entity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.kero.security.core.scheme.AccessScheme;
 
 public class JakartaNameAccessSchemeConfigurator implements AccessSchemeConfigurator {
 
+	private static Logger LOGGER = LoggerFactory.getLogger("Kero-Security-Jakarta");
+	
 	@Override
 	public void configure(AccessScheme scheme) {
 	
@@ -16,10 +21,14 @@ public class JakartaNameAccessSchemeConfigurator implements AccessSchemeConfigur
 			type = type.getSuperclass();
 		}
 		
+		if(!type.isAnnotationPresent(Entity.class)) return;
+
 		Entity annotation = type.getAnnotation(Entity.class);
 		
 		String name = annotation.name();
 		
 		scheme.setAliase(name);
+		
+		LOGGER.info("Fetch scheme aliase: "+name+" from @Entity");
 	}
 }
